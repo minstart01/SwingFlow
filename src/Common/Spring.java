e682173f24700c57f0084d0f63f0a03fd4e7350e
minstart01 <mimiend@naver.com>
2014-08-14 12:48:38 +0900

    다국어 getMessage사용전 setNameSpace 하면 줄여서 사용하기 적용

--- src/Common/Spring.java
+++ src/Common/Spring.java
@@ -8,6 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Spring {
 	private static Locale locale = Locale.getDefault();
 	private static ApplicationContext context = new ClassPathXmlApplicationContext("MultiLanguage.xml");
+	private static String nameSpace="";
 	
 	public static void setLocale(String str){
 		if(null != str) {
@@ -18,11 +19,22 @@ public class Spring {
 			}
 		}
 	}
+	public static void setNameSpace(String para){
+		nameSpace=para;
+	}
+	public static String getNameSpace(){
+		return nameSpace;
+	}
 	public static String getLocale(){
 		return locale.toLanguageTag();
 	}
 	public static String getMessage(String strCode){
-		String ret = context.getMessage(strCode, new Object[0],locale);
+		String ret = null;
+		if((null==nameSpace)||(nameSpace.equals(""))){
+			ret = context.getMessage(strCode, new Object[0],locale);
+		} else {
+			ret = context.getMessage(nameSpace+"."+strCode, new Object[0],locale);
+		}
 		return ret;
 	}
 }
