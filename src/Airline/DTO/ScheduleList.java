2f1c07d9a5627a52c51185688d16befdac99704e
wjdguddlf <godcontrol@naver.com>
2014-09-15 10:39:35 +0900

    리스트 커밋

---
+++ src/Airline/DTO/ScheduleList.java
@@ -0,0 +1,55 @@
+package Airline.DTO;
+
+public class ScheduleList {
+	String a_Name;
+	String an_Name;
+	String c_DeptName;
+	String c_ArrtName;
+	String s_DeptTime;
+	String s_DepDay;
+	int s_Seattotal;
+	public String getA_Name() {
+		return a_Name;
+	}
+	public void setA_Name(String a_Name) {
+		this.a_Name = a_Name;
+	}
+	public String getAn_Name() {
+		return an_Name;
+	}
+	public void setAn_Name(String an_Name) {
+		this.an_Name = an_Name;
+	}
+	public String getC_DeptName() {
+		return c_DeptName;
+	}
+	public void setC_DeptName(String c_DeptName) {
+		this.c_DeptName = c_DeptName;
+	}
+	public String getC_ArrtName() {
+		return c_ArrtName;
+	}
+	public void setC_ArrtName(String c_ArrtName) {
+		this.c_ArrtName = c_ArrtName;
+	}
+	public String getS_DeptTime() {
+		return s_DeptTime;
+	}
+	public void setS_DeptTime(String s_DeptTime) {
+		this.s_DeptTime = s_DeptTime;
+	}
+	public String getS_DepDay() {
+		return s_DepDay;
+	}
+	public void setS_DepDay(String s_DepDay) {
+		this.s_DepDay = s_DepDay;
+	}
+	public int getS_Seattotal() {
+		return s_Seattotal;
+	}
+	public void setS_Seattotal(int s_Seattotal) {
+		this.s_Seattotal = s_Seattotal;
+	}
+	
+	
+}
